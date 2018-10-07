b4aeb10639129b027c8416ddfef513d4313c9d57
vaibhav1510 <vaibhav@chargebee.com>
2018-10-07 13:50:40 +0530

    First commit

---
+++ src/main/java/com/wi/airlines/App.java
@@ -0,0 +1,13 @@
+package com.wi.airlines;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
